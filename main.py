64667f4c60605899c6505bde391ec7a967259bbd
Nathan <152796887+nathandev0@users.noreply.github.com>
2024-03-30 19:25:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+Tinder Automation Bot (iOS, Android)
+Appium-based automated Tinder account creation, profile swiper and bio modification on real devices.
+
+Main functionality:
+1) Device & Automation management system
+2) Account creation (using virtual phone number service API)
+3) Profile swiper (based on set left/right ratio)
+4) Swipe statistics (number of left/right swipes and number of matches)
+5) Bio modification (including checking if there's an instagram handle in bio)
+6) IP rotation system (API proxy rotation link, Airplane mode)
+
+Additional information:
+1) Anti-bot system (human behavior modeling)
+2) No limit on devices (depending on your pc / laptop hardware)
+3) No jailbreak (iOS) / No root (Android) required
+4) Compatible with Windows and MAC OS
+5) GPS location spoofing (*in development)
+
+Contact
+- https://t.me/nathan_dev

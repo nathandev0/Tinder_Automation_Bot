e5616018391628b272df23ed9f2ab8c17b959947
Nathan <152796887+nathandev0@users.noreply.github.com>
2024-04-23 15:17:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ Tinder Automation Bot (iOS, Android)
 Appium-based automated Tinder account creation and swiping on real devices.
 
 Contact:
-https://t.me/nathan_dev0 (new account)
+https://t.me/nathan_dev0
